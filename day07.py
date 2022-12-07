f0d86ffc644b0fb0831a89d62f5c2b5f452cbd24
Anthony Arnold <anthony.arnold@uqconnect.edu.au>
2022-12-07 15:59:51 +1000

    Create day07.py

---
+++ day07.py
@@ -0,0 +1,37 @@
+import sys
+
+
+if __name__ == '__main__':
+    d = ['']  
+    totals = {}
+
+    for line in sys.stdin:
+        if line.startswith('$'):
+            cmd = line[2:]
+            if cmd.startswith('cd'):
+                e = cmd[2:].strip()
+                if e.startswith('/'):
+                    d = ['']
+                elif e == '..':
+                    d.pop()
+                else:
+                    d.append(e)
+        else:
+            x = d.copy()
+            while len(x):
+                f = '/'.join(x)
+                i = line.split()[0].strip()
+
+                if i != 'dir':
+                    s = int(i)
+                    totals[f] = totals.get(f, 0) + s
+                x.pop()
+    
+    print(sum(v for v in totals.values() if v <= 100000))
+
+    avail = 70000000 - totals['']
+    reqd = 30000000 - avail
+
+    print(min(v for v in totals.values() if v > reqd))
+
+
